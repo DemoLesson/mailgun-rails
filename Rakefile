ad44c5c134a58fa2e8d97db1a0df716b39929ec7
Kelly Becker <kellylsbkr@gmail.com>
2012-08-24 17:16:32 -0700

    Version bump to 0.2.0

--- Rakefile
+++ Rakefile
@@ -7,10 +7,10 @@ begin
   Jeweler::Tasks.new do |jewel|
     jewel.name        = 'mailgun-rails'
     jewel.summary     = 'Mailgun adapter for Rails.'
-    jewel.email       = ['tanel.suurhans@perfectline.ee', 'tarmo.lehtpuu@perfectline.ee']
+    jewel.email       = ['tanel.suurhans@perfectline.ee', 'tarmo.lehtpuu@perfectline.ee', 'kbecker@kellybecker.me']
     jewel.homepage    = 'https://github.com/perfectline/mailgun-rails'
     jewel.description = 'Mailgun adapter for Rails.'
-    jewel.authors     = ["Tanel Suurhans", "Tarmo Lehtpuu"]
+    jewel.authors     = ["Tanel Suurhans", "Tarmo Lehtpuu", "Kelly Becker"]
     jewel.files       = FileList["lib/mailgun-rails.rb", "MIT-LICENCE", "README.markdown"]
     jewel.add_dependency 'curb'
     jewel.add_dependency 'activesupport'
--- lib/mailgun-rails.rb
+++ lib/mailgun-rails.rb
@@ -4,60 +4,100 @@ require "curb"
 
 module Mailgun
 
+  # Custom Delivery Error
   class DeliveryError < StandardError
   end
 
+  # Start Delivering
   class DeliveryMethod
 
     attr_accessor :settings
 
+    # Run on initialization
     def initialize(settings)
       self.settings = settings
+
+      # Store tags here
+      @tags = []
     end
 
+    # Get the API_HOST
     def api_host
       self.settings[:api_host]
     end
 
+    # Get the API_KEY
     def api_key
       self.settings[:api_key]
     end
 
+    # Store Tags in an array
+    def tag(*ts)
+      ts.each do |t|
+        @tags << t
+      end
+    end
+
+    # Deliver the message
     def deliver!(mail)
 
-      body              = Curl::PostField.content("message", mail.encoded)
+      # Get the MIME Body
+      body = Curl::PostField.content("message", mail.encoded)
       body.remote_file  = "message"
       body.content_type = "application/octet-stream"
 
+      # Prep the data
       data = []
       data << body
 
+      # Prepare the destinations
       mail.destinations.each do |destination|
         data << Curl::PostField.content("to", destination)
       end
 
-      curl                     = Curl::Easy.new("https://api:#{self.api_key}@api.mailgun.net/v2/#{self.api_host}/messages.mime")
+      # Prepare the tags
+      unless @tags.empty?
+        @tags.each do |t|
+          data << Curl::PostField.content("o:tag", t)
+        end
+      end
+
+      # Create the URL string
+      url = "https://api:#{self.api_key}@api.mailgun.net/v2/#{self.api_host}/messages.mime"
+
+      # Call URL with cURL
+      curl = Curl::Easy.new(url)
       curl.multipart_form_post = true
       curl.http_post(*data)
 
+      # If the result was not successful
       if curl.response_code != 200
 
         begin
+          # Get the error message if another exception is thrown
           error = ActiveSupport::JSON.decode(curl.body_str)["message"]
         rescue
+          # Then output unknown error
           error = "Unknown Mailgun Error"
         end
 
+        # Then raise the new error for debugging
         raise Mailgun::DeliveryError.new(error)
       end
     end
 
+    # Catch the .deliver and reroute
     def self.delivering_email(mail)
-      self.deliver! mail
+
+      # Redirect into Mailgun
+      mail.deliver!
+
+      # Block all additional deliveries
       mail.perform_deliveries = false
     end
   end
 end
 
+# Register the helper
 ActionMailer::Base.add_delivery_method :mailgun, Mailgun::DeliveryMethod
 ActionMailer::Base.register_interceptor Mailgun::DeliveryMethod
